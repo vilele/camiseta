6beb1c9d0efb2994c060f9bba216d2bd09bcda67
vilele <129419657+vilele@users.noreply.github.com>
2024-06-07 13:52:35 -0300

    07/05
    07/05

--- matematica lli/main.js
+++ matematica lli/main.js
@@ -1,3 +1,8 @@
 function comprar(){
-    alert ()
+    const width = 800;
+    const height = 600;
+    const left =(screen.width/2)-(width/2);
+    const top =(screen.height/2)-(height/2);
+    window.open('pagamento.html','pagamento',)
+    width = $(width),height=$(height),top=$(top),left=$(left)
 }
